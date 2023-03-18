750052d58cfa60a4528f9a91bfedf96b79403cce
Gaxyhs <47324415+Gaxihyus@users.noreply.github.com>
2023-03-18 00:27:28 -0300

    changed repulsion and attraction

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var end = false;
 var particles = [];
 var drawLines = false;
+var collisionsOn = true;
 
 var airResistanceMult = 0.01;
 
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
+
 document.getElementById('drawlinesbox').addEventListener('click', () => drawLines = !drawLines)
+document.getElementById('collisionbox').addEventListener('click', () => collisionsOn = !collisionsOn)
 document.getElementById('addParticle').addEventListener('click', () => addParticle());
 document.getElementById('stop').addEventListener('click', () => end = true);
 
@@ -28,35 +32,46 @@ function updateParticle()
         let w = canvas.width;
         let h = canvas.height;
 
-
-        if(p.x + p.vX >= w || p.x + p.vX <= 0) { p.vX *= -1;}
-        if(p.y + p.vY >= h || p.y + p.vY <= 0) { p.vY *= -1;}
+        //Check if out of bounds
+        if(p.x + p.vX >= w || p.x + p.vX <= 0) { p.vX *= -1; p.x = clamp(p.x, 0, w)}
+        if(p.y + p.vY >= h || p.y + p.vY <= 0) { p.vY *= -1; p.y = clamp(p.y, 0, h)};
 
         for(let j = 0; j < particles.length; j++)
         {
             let p2 = particles[j]
             if(p == p2) continue;
             let d = Math.sqrt(Math.pow(p.x - p2.x,2) + Math.pow(p.y - p2.y,2))
-            if(d <= 5)
+            if(d <= p.radius + p2.radius + 2)
             {
-                //p.vX *= -1;
-               // p.vY *= -1;
+                if(collisionsOn)
+                {
+                    p.vX *= -1;
+                    p.vY *= -1;
+                }
             }
             //check if close enough for attraction/repulsion
-            else if(d <= 100)
+            else if(d <= 200)
             {
                 let dx = p2.x - p.x;
                 let dy = p2.y - p.y;
-                const Fx = 1 / dx;
-                const Fy = 1 / dy;
+                let Fx = 5 / dx;
+                let Fy = 5 / dy;
+
+                if(p.color != p2.color)
+                {
+                    Fx *= -1;
+                    Fy *= -1;
+                }
 
                 p.vX += Fx;
-                p.vY += Fy
+                p.vY += Fy;
 
-                if(p.vX > 2) p.vX = 2;
-                if(p.vX < -2) p.vX = -2;
-                if(p.vY > 2) p.vY = 2;
-                if(p.vY < -2) p.vY = -2;
+                let maxSpeed = 2;
+
+                if(p.vX > maxSpeed) p.vX = maxSpeed;
+                if(p.vX < -maxSpeed) p.vX = -maxSpeed;
+                if(p.vY > maxSpeed) p.vY = maxSpeed;
+                if(p.vY < -maxSpeed) p.vY = -maxSpeed;
             }
         }
 
@@ -76,7 +91,7 @@ function drawParticles()
     {
         let nextP = particles[(i + 1) % particles.length]
         let p = particles[i];
-        drawCircle(p.x, p.y, 2.5, p.color)
+        drawCircle(p.x, p.y, p.radius, p.color)
         if(drawLines === true)
         {
             ctx.beginPath();
@@ -113,7 +128,7 @@ function drawCircle(x, y, radius, color)
     ctx.fill();
 }
 
-function addParticle()
+function addParticle(color = undefined)
 {
     let w = canvas.width;
     let h = canvas.height;
@@ -124,20 +139,31 @@ function addParticle()
     let vx = Math.random();
     let vy = Math.random();
 
-    let p = new Particle(x,y,vx,vy, getRandomColor());
+    let radius = 2 + Math.random() * 5;
+
+    if(color == undefined) color = getRandomColor();
+
+    let p = new Particle(x,y,vx,vy, radius, color);
 
     particles.push(p);
 }
 
 class Particle{
-    constructor(x, y, vX, vY, color)
+    constructor(x, y, vX, vY, radius, color)
     {
         this.x = x;
         this.y = y;
         this.vX = vX;
         this.vY = vY;
+        this.radius = radius;
         this.color = color;
     }
+}
+
+for(let i = 0; i < 250; i++)
+{
+    addParticle('#FF0000');
+    addParticle();
 }
 
 animate();
